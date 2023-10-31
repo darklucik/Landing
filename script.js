e536e951759d2fa04855b3b6a94965caebeeb33d
darklucik <rustamzonovsultan3@gmail.com>
2023-10-31 22:04:25 +0500

    second hm

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let firstNum = Number(prompt("Выберите первое число"))
+let symbol = prompt("Выберите действие(+ - * /)")
+let secondNum = Number(prompt("Выберите второе число"))
 
+if (symbol=="+") {
+        alert(firstNum + secondNum)
+    }
+    if (symbol=="-") {
+        alert(firstNum - secondNum)
+    }
+    if (symbol=="*") {
+        alert(firstNum * secondNum)
+    }
+    if (symbol=="/") {
+        alert(firstNum / secondNum)
+    }
